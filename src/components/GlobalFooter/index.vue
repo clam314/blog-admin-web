287877a80cd8caf24be1c94f43ea31d7674aff90
woods <clam314@163.com>
2021-06-03 13:34:06 +0800

    fix:修复底部blog跳转问题

--- src/components/GlobalFooter/index.vue
+++ src/components/GlobalFooter/index.vue
@@ -1,7 +1,7 @@
 <template>
   <global-footer class="footer custom-render">
     <template v-slot:links>
-      <a href="" target="_blank">Clam's blog</a>
+      <a href="/" target="_blank">Blog</a>
       <a href="https://github.com/clam314/blog-admin-web.git" target="_blank">Github</a>
       <a href="https://github.com/clam314" target="_blank">@clam314</a>
     </template>
